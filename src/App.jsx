2a64bdf7b5a4d1c2937fc954cbe2bce0fba7eb5c
mykolas555 <mykolas.motuzas@stud.kitm.lt>
2024-07-05 18:44:55 +0300

    Go reads api unfinished

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Register from './components/authorization/Register';
 
 function App() {
   return (
-    <Router basename={LOGIN_URL}>
+    <Router>
       <Header />
       <Routes>
         <Route path="/register" element={<Register />} />
